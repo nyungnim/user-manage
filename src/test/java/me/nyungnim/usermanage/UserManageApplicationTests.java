be28a05952cb46895779c9ef8a50b30359c446f5
namalleng <nyu0kim@gmail.com>
2024-11-09 16:56:44 +0900

    프로젝트 생성

---
+++ src/test/java/me/nyungnim/usermanage/UserManageApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nyungnim.usermanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
